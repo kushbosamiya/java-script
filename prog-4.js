b592537bc4cb1eebccb9988b7b0ef1c5b572cef3
kush <kushalbosamiya13@gmail.com>
2020-11-20 23:57:31 +0530

    loops in js

---
+++ prog-4.js
@@ -0,0 +1,20 @@
+// loops in js
+/* this for loops is works as works in c lang. and have same syntax*/
+
+// exercise-9
+
+
+for (let i = 0; i < 5; i++) {
+    console.log("kush");
+}
+
+
+// example
+for(i=0;i<10;i++)
+{
+for(j=22;j<i;j++)
+{
+    
+}
+console.log("sum:",j+i);
+}
